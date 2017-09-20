67138aea5c2f483ea4227276049ccd38cb86b2bf
Екатерина <@LENOVO-PC>
2017-09-20 21:36:49 +0500

    всё

--- Task_4/Task_4/Program.cs
+++ Task_4/Task_4/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_4
 {
@@ -10,12 +6,30 @@ namespace Task_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите точность для вычисления суммы ряда, заданного формулой (задание 4-г)...");
-
-            double e = Convert.ToDouble(Console.ReadLine()), count = 0, temp = Math.Pow(-2, 1) / 1;
+            Console.WriteLine("Введите точность для вычисления суммы ряда (e > 0) ...");
+            bool ok = false;
+            double e =0;// заданная точность
+            // проверка корректности ввода
+            while (ok == false)
+            {
+                //попытка конвертировать с постусловием
+                try
+                {
+                    e = Convert.ToDouble(Console.ReadLine());
+                    if (e > 0) ok = true;
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Некорректный ввод данных. Попробуйте ещё...");
+                }
+            }
+            //счетчик суммы  и первый элемент для сравнения
+            double count = 0, temp = Math.Pow(-2, 1) / 1;
+            //факториал
             int fkt = 1;
+            //количество подчитанных элементов
             int KolElem = 0;
-
+            //реализация алгоритма составления ряда
             for (int i = 1; Math.Abs(temp) >= e; i++)
             {
                 fkt *= i;
@@ -24,10 +38,8 @@ namespace Task_4
                 Console.WriteLine(temp);
                 KolElem++;
             }
-
             Console.WriteLine("Сумма " + KolElem + " элементов равна " + count);
             Console.ReadLine();
-
         }
     }
 }
